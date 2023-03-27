14dae7cf0d7df1e2b530cd80777ba9a55fa812c6
PatrickVGould <125094430+PatrickVGould@users.noreply.github.com>
2023-03-27 13:20:58 +1100

    Update main.py

--- main.py
+++ main.py
@@ -5,48 +5,81 @@ st.title("Positive Cardiometabolic Health Resource")
 st.subheader("An early intervention framework for people on psychotropic medication")
 
 # Demographic questions
-sex = st.radio("What is the person's sex?", ["Male", "Female", "Other"])
-age = st.number_input("What is the person's age?")
-ethnicity = st.radio("Is the person of any of the following ethnicities? South Asian, Chinese, Japanese, Ethnic South and Central Americans", ["Yes", "No"])
+st.header("Demographics")
+col1, col2, col3 = st.columns(3)
+with col1:
+    sex = st.radio("What is the person's sex?", ["Male", "Female", "Other"])
+with col2:
+    age = st.number_input("What is the person's age?")
+with col3:
+    ethnicity = st.radio("Is the person of any of the following ethnicities? South Asian, Chinese, Japanese, Ethnic South and Central Americans", ["Yes", "No"])
 
 # Lifestyle questions
 st.header("Lifestyle")
-smoking = st.radio("Is the person a current smoker (including tobacco, THC, vaping, shisha)?", ["Yes", "No"])
-diet = st.radio("Does the person have a poor diet?", ["Yes", "No"])
-activity = st.radio("Does the person have more than 30 minutes physical activity on most days?", ["Yes", "No"])
+col1, col2, col3 = st.columns(3)
+with col1:
+    smoking = st.radio("Is the person a current smoker (including tobacco, THC, vaping, shisha)?", ["Yes", "No"])
+with col2:
+    diet = st.radio("Does the person have a poor diet?", ["Yes", "No"])
+with col3:
+    activity = st.radio("Does the person have more than 30 minutes physical activity on most days?", ["Yes", "No"])
 
 # Obesity questions
 st.header("Obesity")
-BMI = st.number_input("What is the person's BMI?")
-weight_increase = st.radio("Has the person had a weight increase of greater than 5kg over the past 3 months?", ["Yes", "No"])
-waist_circ = st.number_input("What is the person's waist circumference in cm?")
-waist_increase = st.radio("Has the person had an increase in waist circumference of over 5cm in the past 3 months?", ["Yes", "No"])
+col1, col2, col3 = st.columns(3)
+with col1:
+    BMI = st.number_input("What is the person's BMI?")
+with col2:
+    weight_increase = st.radio("Has the person had a weight increase of greater than 5kg over the past 3 months?", ["Yes", "No"])
+with col3:
+    waist_circ = st.number_input("What is the person's waist circumference in cm?")
+col1, col2 = st.columns(2)
+with col1:
+    waist_increase = st.radio("Has the person had an increase in waist circumference of over 5cm in the past 3 months?", ["Yes", "No"])
 
 # Blood pressure questions
 st.header("Blood Pressure")
-systolic_bp = st.number_input("What is the person's systolic blood pressure?")
-diastolic_bp = st.number_input("What is the person's diastolic blood pressure?")
+col1, col2 = st.columns(2)
+with col1:
+    systolic_bp = st.number_input("What is the person's systolic blood pressure?")
+with col2:
+    diastolic_bp = st.number_input("What is the person's diastolic blood pressure?")
 
 # Glucose questions
 st.header("Glucose")
-HbA1c = st.number_input("What is the person's HbA1c?")
-FPG = st.number_input("What is the person's FPG?")
-AUSDRISK = st.number_input("What is the person's AUSDRISK?")
+col1, col2, col3 = st.columns(3)
+with col1:
+    HbA1c = st.number_input("What is the person's HbA1c?")
+with col2:
+    FPG = st.number_input("What is the person's FPG?")
+with col3:
+    AUSDRISK = st.number_input("What is the person's AUSDRISK?")
 
 # Blood lipids questions
 st.header("Blood Lipids")
-TC = st.number_input("What is the person's TC?")
-LDL = st.number_input("What is the person's LDL?")
-HDL = st.number_input("What is the person's HDL?")
-RPG = st.number_input("What is the person's RPG?")
-non_HDL = st.number_input("What is the person's non-HDL?")
-TRIG = st.number_input("What is the person's TRIG?")
+col1, col2, col3, col4, col5, col6 = st.columns(6)
+with col1:
+    TC = st.number_input("What is the person's TC?")
+with col2:
+    LDL = st.number_input("What is the person's LDL?")
+with col3:
+    HDL = st.number_input("What is the person's HDL?")
+with col4:
+    RPG = st.number_input("What is the person's RPG?")
+with col5:
+    non_HDL = st.number_input("What is the person's non-HDL?")
+with col6:
+    TRIG = st.number_input("What is the person's TRIG?")
 
 # Sleep questions
 st.header("Sleep")
-neck_circ = st.number_input("What is the person's neck circumference in cm?")
-daytime_tiredness = st.radio("Does the person have daytime tiredness?", ["Yes", "No"])
-snoring = st.radio("Does the person have loud snoring or stop breathing during sleep?", ["Yes", "No"])
+col1, col2, col3 - st.columns(3)
+with col1:
+    neck_circ = st.number_input("What is the person's neck circumference in cm?")
+with col2:
+    daytime_tiredness = st.radio("Does the person have daytime tiredness?", ["Yes", "No"])
+with col3:
+    snoring = st.radio("Does the person have loud snoring or stop breathing during sleep?", ["Yes", "No"])
 
 # Submit button
 submit = st.button("Submit")
